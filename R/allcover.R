caa93634f92e5bf9b581be8028f7f27dfb635f20
John E. Kolassa <kolassa@stat.rutgers.edu>
2023-09-14 09:47:17 -0400

    Work around error in all cover (temporarily, maybe)

--- R/allcover.R
+++ R/allcover.R
@@ -4,7 +4,7 @@
 #' @param npts number of potential nominal coverages to consider
 #' @param plotdat optional saved output for replotting.
 #' @export
-#' @importFrom MultNonParam util.jplot
+##' @importFrom MultNonParam util.jplot
 fun.allcover<-function(nn,larange=c(-6,-1),npts=50000,plotdat=NULL){
    if(is.null(plotdat)){
       plotdat<-list( lalphav=larange[1]+(0:npts)*diff(larange)/npts,
@@ -15,7 +15,11 @@ fun.allcover<-function(nn,larange=c(-6,-1),npts=50000,plotdat=NULL){
       keep<-c(TRUE,diff(plotdat$out)!=0)|c(diff(plotdat$out)!=0,TRUE)
       plotdat$lalphav<-plotdat$lalphav[keep] ; plotdat$out<-plotdat$out[keep]
    }
-   util.jplot(1-exp(plotdat$lalphav),plotdat$out,xlab="Nominal Coverage",
+#  util.jplot(1-exp(plotdat$lalphav),plotdat$out,xlab="Nominal Coverage",
+#     main="True Coverage for Various Nominal Targets",
+#     ylab="True Coverage",sub=paste("Number of Binomial Trials",nn))
+   plot(1-exp(plotdat$lalphav),plotdat$out,xlab="Nominal Coverage",
+      type="l",
       main="True Coverage for Various Nominal Targets",
       ylab="True Coverage",sub=paste("Number of Binomial Trials",nn))
    abline(0,1,lty=2)
